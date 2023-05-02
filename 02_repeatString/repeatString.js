0d4de71bb2b2b3998a9a9ea0409000719d5a324a
implObserver <prognozman@mail.ru>
2023-05-02 23:29:13 +0500

    feat: add code that passes the test 5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
 const repeatString = function (word, quantity) {
+    if (quantity < 0) {
+        return 'ERROR';
+    }
     return word.repeat(quantity);
 };
-
 // Do not edit below this line
 module.exports = repeatString;
