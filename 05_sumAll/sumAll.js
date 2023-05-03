6756fe5bbf96ca5a3634b03d3d80fa1f7715e758
implObserver <prognozman@mail.ru>
2023-05-03 21:02:56 +0500

    feat:add a solution
    add a solution that passes all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
 
+
+const sumAll = function (num1, num2) {
+    let sum = 0;
+    let start = num1 > num2 ? num2 : num1;
+    let end = num1 > num2 ? num1 : num2;
+
+    if (typeof start === 'number'
+        && typeof end === 'number'
+        && start >= 0
+        && end >= 0) {
+            
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+
+    } else {
+        return 'ERROR';
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
