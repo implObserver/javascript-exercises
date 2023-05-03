f06923937cb7a0b4e39834dfa583721922f09594
implObserver <prognozman@mail.ru>
2023-05-03 23:37:19 +0500

    feat:add a solution
    add a solution 09_palindromes that passes all the tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g, "").toLowerCase();
+    let reverseCleanStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reverseCleanStr;
 };
 
 // Do not edit below this line
