dca32c8400ac93f607fa7bda6b022b29780bfc08
implObserver <prognozman@mail.ru>
2023-05-03 21:17:50 +0500

    feat:add a solution
    add a solution 06_leapYears that passes all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,29 @@
-const leapYears = function() {
+function isMultiple4(num) {
+    return num % 4 === 0;
+}
 
+function isMultiple100(num) {
+    return num % 100 === 0;
+}
+
+function isMultiple400(num) {
+    return num % 400 === 0;
+}
+
+const leapYears = function (year) {
+    if (isMultiple4(year)) {
+        if (isMultiple100(year)) {
+            if (isMultiple400(year)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
