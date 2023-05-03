894a7d63ccf08d0acd4ddf36ca49c77556e362a2
implObserver <prognozman@mail.ru>
2023-05-03 23:20:23 +0500

    feat:add a solution
    add a solution 08_calculator that passes all the tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let result = arr.length > 1
+    ? arr.reduce((arg, nextarg) => arg + nextarg, 0)
+    : arr.length === 0 ? 0
+      : arr[0];
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result = arr.length > 1
+    ? arr.reduce((arg, nextarg) => arg * nextarg)
+    : arr.length === 0 ? 0
+      : arr[0];
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  return Math.pow(num, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
+      result *= i;
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
