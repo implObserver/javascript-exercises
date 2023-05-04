5466ddba427ab4b7ee91299407871f37517c84b9
implObserver <prognozman@mail.ru>
2023-05-04 12:26:15 +0500

    add solution
    add solution 12_findTheOldest that has passed all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    let processedArr = arr.map(e => [e.name, e.yearOfDeath >= 0
+        ? e.yearOfDeath - e.yearOfBirth
+        : new Date().getFullYear() - e.yearOfBirth]);
 
+    let oldestHuman = processedArr.reduce((accumulator, element) => {
+        return element[1] > accumulator[1] ? element : accumulator;
+    });
+
+    let result = {
+        name: oldestHuman[0],
+        age: oldestHuman[1],
+    };
+
+    return result;
 };
 
 // Do not edit below this line
