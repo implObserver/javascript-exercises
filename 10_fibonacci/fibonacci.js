ef176458cd0a86fbc633f93aecb41172aaa599c1
implObserver <prognozman@mail.ru>
2023-05-04 11:08:42 +0500

    add solution
    add solution 10_fibonacci that has passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
 
+    if (num < 0) {
+        return 'OOPS';
+    }
+
+    let result = parseInt(num);
+    let previousFib = 0;
+    let currentFib = 1;
+
+    for (let i = 2; i <= num; i++) {
+        result = previousFib + currentFib;
+        previousFib = currentFib;
+        currentFib = result;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
